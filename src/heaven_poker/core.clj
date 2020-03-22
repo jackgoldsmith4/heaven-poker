b50c5e1d227801be0f92de2fb6b823d452e28970
Jack Goldsmith <jackgoldsmith4@gmail.com>
2020-03-22 18:45:56 -0400

    manipulate data to include player data map

--- src/heaven_poker/core.clj
+++ src/heaven_poker/core.clj
@@ -1,7 +1,8 @@
 (ns heaven-poker.core
   (:require [heaven-poker.deck :refer :all])
   (:require [heaven-poker.hand :refer :all])
-  (:require [heaven-poker.to-string :refer :all]))
+  (:require [heaven-poker.to-string :refer :all])
+  (:use clojure.data))
 
 (defn main
   [& players]
@@ -15,17 +16,14 @@
   ; 5 community cards: flop, turn, river
   (def community-cards (take 5 (reverse relevant-cards)))
 
-  ; a map of the strength and five-card "made hand" for each player's hand combined with the community cards
-  (def hand-ranks
-    (map (fn [player]
-           (let [hand (concat (:hand player) community-cards)]
-             (rank-hand hand))) players-with-hands))
+  ; computes hand rankings and creates a larger map of player data to include these hand rankings
+  (def player-data (map #(assoc %1 :hand-ranking %2) players-with-hands (map #(rank-hand (concat (:hand %) community-cards)) players-with-hands)))
 
-  (println (map (fn [player] (str (:name player) "'s hand: " (starting-hand-to-string (:hand player)) "\n")) players-with-hands)
+  (println (map (fn [player] (str (:name player) "'s hand: " (starting-hand-to-string (:hand player)) "\n")) player-data)
            "\n\nCommunity cards:\n"
            (map (fn [card] (str (card-to-string card) "\n")) community-cards)
            "\n\nHand Ranks:\n"
-           (map (fn [rank] (str (nth players (.indexOf hand-ranks rank)) ": "
-                                (hand-ranking-to-string rank) "\n")) hand-ranks)
+           (map (fn [player] (str (:name player) ": " (hand-ranking-to-string (:hand-ranking player)) "\n")) player-data)
            "\n\nWinner: "
-           (hand-ranking-to-string (first (compare-hand-ranks hand-ranks)))))
+           (:name (first (filter (fn [player] (let [winner (diff (:hand-ranking player) (first (compare-hand-ranks (map #(:hand-ranking %) player-data))))]
+                                                (and (nil? (first winner)) (nil? (first (rest winner)))))) player-data)))))
