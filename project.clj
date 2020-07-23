fdf0a18c06ad82d08039d03a1483e8b5d98d4323
dennycmiel <42587254+dennycmiel@users.noreply.github.com>
2020-07-23 09:53:14 -0700

    Add local browser setup

--- project.clj
+++ project.clj
@@ -3,6 +3,13 @@
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler heaven-poker.core/home
+         :auto-reload? true
+         :auto-refresh false}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring "1.8.1"]
+                 [hiccup "2.0.0-alpha2"]
+                 [compojure "1.6.1"]]
   :repl-options {:init-ns heaven-poker.core}
   :main heaven-poker.core/main)
